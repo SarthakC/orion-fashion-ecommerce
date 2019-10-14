ee0f25325eddde4ba07a85c97049040aed6b6dd8
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-10-14 21:26:13 +0530

    reduced homepage to different components, finished styling the menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.scss';
+import Directory from '../../components/Directory/Directory';
+
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
