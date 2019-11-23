013be754eac597514e607db69317a682ce82d631
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-11-24 02:14:15 +0530

    [integraded] redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+
+import store from './redux/store';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
