c23aefae419ce2710ec6b47ecbf3c9f8c4c310bc
Sarthak Chowdhary <sarthak.chow@gmail.com>
2019-11-28 11:42:03 +0530

    [added] session storage+ persistence using 'redux-persist'

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById('root'),
